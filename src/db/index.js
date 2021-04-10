7673521dfecd5f88ba98c350a653b6b4bfe7b45b
aniledev <elinamcgill@gmail.com>
2021-04-10 05:02:16 -0400

    CHORE: add pseudocode for future heroku deployment and postgres provisioning

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
+const { PORT, DATABASE_URL } = require("../config");
+// const connectionString = the url of the deployed postres database on heroku
 
 const pool = new Pool();
-
+// Pool({conectionString})
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
