5e54a71da434df6c2f6344853d50bccfddd79413
aniledev <elinamcgill@gmail.com>
2021-04-10 02:41:58 -0400

    CHORE: update server file

--- src/server.js
+++ src/server.js
@@ -15,5 +15,5 @@ const db = knex({
 app.set("db", db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Bon Appetit Server listening at http://localhost:${PORT}`);
 });
