2277bbc6d82886a2622b490680750e8cd9db2bd6
aniledev <elinamcgill@gmail.com>
2021-04-10 04:24:57 -0400

    CHORE: configure node-postgres to connect to to postgres database

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,13 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+
+const pool = new Pool({
+  user: "elinamcgill",
+  host: "localhost",
+  database: "bon-appetit",
+  //   password: "secretpassword",
+  port: 5432,
+});
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
