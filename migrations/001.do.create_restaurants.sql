a5e2df52e73d05b8581687ec82667b82a339f27d
aniledev <elinamcgill@gmail.com>
2021-04-10 02:36:41 -0400

    FEAT: add constraints to price range property in restaurants table

--- migrations/001.do.create_restaurants.sql
+++ migrations/001.do.create_restaurants.sql
@@ -4,5 +4,5 @@ CREATE TABLE "restaurants"
     "id" SERIAL PRIMARY KEY NOT NULL,
     "name" VARCHAR(50) NOT NULL,
     "location" VARCHAR(50) NOT NULL,
-    "price_range" INT NOT NULL
+    "price_range" INT NOT NULL check(price_range >= 1 and price_range <=5)
 );
