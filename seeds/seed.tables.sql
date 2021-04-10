d21102e13a11f696cb29839bdf93d390bfecc834
aniledev <elinamcgill@gmail.com>
2021-04-10 02:31:57 -0400

    FEAT: update seed file to reflect serial sequence id in migration file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -4,18 +4,18 @@ BEGIN;
     TRUNCATE TABLE "restaurants";
 
     INSERT INTO "restaurants"
-        ("id", "name", "location", "price_range")
+        ("name", "location", "price_range")
 
     VALUES
-        (1, 'Le Bernardin', 'New York', 4),
-        (2, 'Due Amici Pizza', 'Tampa', 1),
-        (3, 'El Five', 'Denver', 3),
-        (4, 'Born and Raised', 'San Diego', 4),
-        (5, 'Riel', 'Houston', 3),
-        (6, 'Adda', 'New York', 2),
-        (7, 'Gemma', 'Dallas', 2),
-        (8, 'Se7en Bites', 'Orlando', 2),
-        (9, 'Epicure', 'Paris', 5),
-        (10, 'Brawn', 'London', 2);
+        ('Le Bernardin', 'New York', 4),
+        ('Due Amici Pizza', 'Tampa', 1),
+        ('El Five', 'Denver', 3),
+        ('Born and Raised', 'San Diego', 4),
+        ('Riel', 'Houston', 3),
+        ('Adda', 'New York', 2),
+        ('Gemma', 'Dallas', 2),
+        ('Se7en Bites', 'Orlando', 2),
+        ('Epicure', 'Paris', 5),
+        ('Brawn', 'London', 2);
 
     COMMIT;
