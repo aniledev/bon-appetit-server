eef5ba28ccaf5835b0354244e057d6ca7757a6a1
aniledev <elinamcgill@gmail.com>
2021-04-11 08:15:41 -0400

    REFRACTOR: change SQL query to display number of ratings for each restaurant

--- src/app.js
+++ src/app.js
@@ -31,8 +31,12 @@ app.use(express.json());
 // create an endpoint for GET restaurants
 app.get("/api/restaurant", async (req, res) => {
   try {
-    const response = await db.query("SELECT * FROM restaurants");
-    // console.log(response);
+    // const response = await db.query("SELECT * FROM restaurants");
+
+    const response = await db.query(
+      "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
+    );
+    // console.log(response["rows"]);
     res.status(200).json({
       response: response.rows.length,
       data: { restaurants: response["rows"] },
