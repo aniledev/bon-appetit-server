74886d0d168729bcf13e2406908c442cb8f5e07d
aniledev <elinamcgill@gmail.com>
2021-04-11 09:09:30 -0400

    REFRACTOR: move GET /api/restaurant/:id endpoint into router

--- src/app.js
+++ src/app.js
@@ -46,31 +46,31 @@ app.use("/api/restaurant", restaurantRouter);
 
 // create an endpoint for GET a singular restaurant
 
-app.get("/api/restaurant/:id", async (req, res) => {
-  // use the req.params to get the of the restaurant we want to get
-  try {
-    const response = await db.query(
-      "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurants.id = reviews.restaurant_id WHERE id = $1",
-      [req.params.id]
-    );
+// app.get("/api/restaurant/:id", async (req, res) => {
+//   // use the req.params to get the of the restaurant we want to get
+//   try {
+//     const response = await db.query(
+//       "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurants.id = reviews.restaurant_id WHERE id = $1",
+//       [req.params.id]
+//     );
 
-    // make another request to get related reviews for a single restauran
-    const reviews = await db.query(
-      "SELECT * FROM reviews WHERE restaurant_id = $1",
-      [req.params.id]
-    );
+//     // make another request to get related reviews for a single restauran
+//     const reviews = await db.query(
+//       "SELECT * FROM reviews WHERE restaurant_id = $1",
+//       [req.params.id]
+//     );
 
-    // console.log(response["rows"]);
-    // console.log(reviews.rows);
-    res.status(200).json({
-      response: response.rows.length,
-      data: { restaurant: response.rows[0], reviews: reviews.rows },
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  // SELECT FROM * restaurants WHERE id = 1; <-- selecting a specific restaurant based on the id
-});
+//     // console.log(response["rows"]);
+//     // console.log(reviews.rows);
+//     res.status(200).json({
+//       response: response.rows.length,
+//       data: { restaurant: response.rows[0], reviews: reviews.rows },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   // SELECT FROM * restaurants WHERE id = 1; <-- selecting a specific restaurant based on the id
+// });
 
 // create an endpoint for POST a single restaurant
 // app.post("/api/restaurant", async (req, res) => {
--- src/restaurant/restaurantRouter.js
+++ src/restaurant/restaurantRouter.js
@@ -21,6 +21,31 @@ restaurantRouter
       console.log(error);
     }
   })
+  .get("/:id", async (req, res) => {
+    // use the req.params to get the of the restaurant we want to get
+    try {
+      const response = await db.query(
+        "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating FROM reviews GROUP BY restaurant_id) reviews on restaurants.id = reviews.restaurant_id WHERE id = $1",
+        [req.params.id]
+      );
+
+      // make another request to get related reviews for a single restauran
+      const reviews = await db.query(
+        "SELECT * FROM reviews WHERE restaurant_id = $1",
+        [req.params.id]
+      );
+
+      // console.log(response["rows"]);
+      // console.log(reviews.rows);
+      res.status(200).json({
+        response: response.rows.length,
+        data: { restaurant: response.rows[0], reviews: reviews.rows },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    // SELECT FROM * restaurants WHERE id = 1; <-- selecting a specific restaurant based on the id
+  })
   .post("/", async (req, res) => {
     //destruture the req.body to send request to the server
     // wrap your async actions in try catch block with simple error handling
