e9bf295bb885f080b4e78bcfb39c7de2dff8e0ff
aniledev <elinamcgill@gmail.com>
2021-04-11 09:11:06 -0400

    REFRACTOR: move reviews endpoint into router

--- src/restaurant/restaurantRouter.js
+++ src/restaurant/restaurantRouter.js
@@ -64,6 +64,23 @@ restaurantRouter
       console.log(error);
     }
     // return the appropriate data back in a request
+  })
+  .post("/:id/review", async (req, res) => {
+    try {
+      const response = await db.query(
+        "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
+        [req.params.id, req.body.name, req.body.review, req.body.rating]
+      );
+      // console.log(response);
+      res.status(201).json({
+        response: response.rows.length,
+        data: {
+          review: response.rows[0],
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 
 module.exports = restaurantRouter;
