b3d8a0db6fb27bff1e25df38c331b37a746c3924
aniledev <elinamcgill@gmail.com>
2021-04-27 01:14:55 -0400

    REFRACTOR: remove unecessary variables for readability

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,7 @@
 const { Pool } = require("pg");
-const connectionString =
-  "postgres://ljqigomdponytb:28ab70fda30ef7c7ab1f41492aedd1f88f7ab4f21b008b13acbd6122ca74923f@ec2-52-1-115-6.compute-1.amazonaws.com:5432/dcpl4todj3b45f";
+const { DATABASE_URL } = require("../config");
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ DATABASE_URL });
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
