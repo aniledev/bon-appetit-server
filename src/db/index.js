7d41c2de87bb1b9a50aaaa61c85781b952b63f75
aniledev <elinamcgill@gmail.com>
2021-04-27 17:26:11 -0400

    CHORE: update pg config

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
-const { DATABASE_URL } = require("../config");
 
-const pool = new Pool({ DATABASE_URL });
+const connectionString =
+  "postgres://ljqigomdponytb:28ab70fda30ef7c7ab1f41492aedd1f88f7ab4f21b008b13acbd6122ca74923f@ec2-52-1-115-6.compute-1.amazonaws.com:5432/dcpl4todj3b45f";
+
+const pool = new Pool({ connectionString });
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
